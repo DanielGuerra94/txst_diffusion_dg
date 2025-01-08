9e96204422766e61879c7d2799545e0a7806631e
DanielGuerra94 <dag143@txstate.edu>
2025-01-08 22:36:02 +0000

    Start modularizing and testing diffusion model

--- diffusion.py
+++ diffusion.py
@@ -1,27 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+def calculate_time_step(grid_spacing, diffusivity):
+    return 0.5 * grid_spacing**2 / diffusivity
+
+
 D = 100 # diffusivity
 Lx = 300 # length of grid
 dx = 0.5 # step size
+
 x = np.arange(start=0, stop=Lx, step=dx) # array
 nx = len(x) # array length
+
 C = np.zeros_like(x) #creates array filled with zeros mirroring array x
 C_left = 500
 C_right = 0
 C[x <= Lx//2] = C_left # // returns integers only
 C[x > Lx//2] = C_right
+
 plt.figure()
 plt.plot(x,C,"r")
 plt.xlabel("x")
 plt.ylabel("C")
 plt.title("Initial concentration profile")
+
 time = 0
 nt = 5000
-dt = 0.5 * dx**2 / D # ** = ^ in R
+dt = calculate_time_step(dx,D)
+
 for t in range(0, nt):
     C += D * dt / dx**2 * (np.roll(C, -1) - 2*C + np.roll(C, 1)) # += adds current value to result
     C[0] = C_left
     C[-1] = C_right
+
 plt.figure()
 plt.plot(x, C, "b")
 plt.xlabel("x")
